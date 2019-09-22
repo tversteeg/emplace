a65d032a1997e29a5328decafcd15ea934e98f71
Thomas Versteeg <thomasversteeg@gmx.com>
2019-09-22 21:20:51 +0200

    Try to release binary files with Travis CI

--- ci/before_deploy.sh
+++ ci/before_deploy.sh
@@ -3,31 +3,13 @@
 set -ex
 
 main() {
-    local src=$(pwd) \
-          stage=
-
-    case $TRAVIS_OS_NAME in
-        linux)
-            stage=$(mktemp -d)
-            ;;
-        osx)
-            stage=$(mktemp -d -t tmp)
-            ;;
-    esac
+    local src=$(pwd)
 
     test -f Cargo.lock || cargo generate-lockfile
 
-    # TODO Update this to build the artifacts that matter to you
-    cross rustc --bin hello --target $TARGET --release -- -C lto
+    cross rustc --bin emplace --target $TARGET --release -- -C lto
 
-    # TODO Update this to package the right artifacts
-    cp target/$TARGET/release/hello $stage/
-
-    cd $stage
-    tar czf $src/$CRATE_NAME-$TRAVIS_TAG-$TARGET.tar.gz *
-    cd $src
-
-    rm -rf $stage
+    cp target/$TARGET/release/emplace $src/
 }
 
 main
