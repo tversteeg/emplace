6c8e224bb6dbca89ade6bf677a955ecd916c7ee9
Thomas Versteeg <thomasversteeg@gmx.com>
2019-09-22 20:48:33 +0200

    Try to let AppVeyor CI deploy the executable

--- ci/before_deploy.ps1
+++ ci/before_deploy.ps1
@@ -8,14 +8,10 @@ Set-Location $ENV:Temp
 New-Item -Type Directory -Name $STAGE
 Set-Location $STAGE
 
-$ZIP = "$SRC_DIR\$($Env:CRATE_NAME)-$($Env:APPVEYOR_REPO_TAG_NAME)-$($Env:TARGET).zip"
+$EXE = "$SRC_DIR\$($Env:CRATE_NAME)-$($Env:APPVEYOR_REPO_TAG_NAME)-$($Env:TARGET).exe"
+Copy-Item "$SRC_DIR\target\$($Env:TARGET)\release\emplace.exe" "$EXE"
 
-# TODO Update this to package the right artifacts
-Copy-Item "$SRC_DIR\target\$($Env:TARGET)\release\hello.exe" '.\'
-
-7z a "$ZIP" *
-
-Push-AppveyorArtifact "$ZIP"
+Push-AppveyorArtifact "$EXE"
 
 Remove-Item *.* -Force
 Set-Location ..
