d883a3920033135cecea8f4cf8c2ad0dddb92ffc
James Trudel <jtrudel@ea.com>
2021-04-30 14:36:38 -0700

    Add support for OpenSuse
    Added zypper.rs implementation
    Added entry to mod.rs for zypper
    Updated README.md

--- src/package_manager/mod.rs
+++ src/package_manager/mod.rs
@@ -20,6 +20,7 @@ mod rustup;
 mod scoop;
 mod snap;
 mod yay;
+mod zypper;
 
 pub use apt::Apt;
 pub use brew::Brew;
@@ -40,6 +41,7 @@ pub use rustup::Rustup;
 pub use scoop::Scoop;
 pub use snap::Snap;
 pub use yay::Yay;
+pub use zypper::Zypper;
 
 use serde::{Deserialize, Serialize};
 use std::path::PathBuf;
@@ -70,6 +72,7 @@ pub enum PackageManager {
     Scoop,
     Snap,
     Yay,
+    Zypper,
 }
 
 /// Trait that needs to be implemented for a new package manager.
---
+++ src/package_manager/zypper.rs
@@ -0,0 +1,72 @@
+use super::{CaptureFlag, PackageInstalledMethod, PackageManagerTrait};
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Default, Copy, Clone, Eq, PartialEq, Serialize, Deserialize)]
+pub struct Zypper;
+
+impl PackageManagerTrait for Zypper {
+    fn full_name(self) -> &'static str {
+        "Command-line interface to ZYpp system management library"
+    }
+
+    fn commands(self) -> Vec<&'static str> {
+        vec!["zypper"]
+    }
+
+    fn sub_commands(self) -> Vec<&'static str> {
+        vec!["install"]
+    }
+
+    fn install_command(self) -> &'static str {
+        "zypper install -y"
+    }
+
+    fn needs_root(self) -> bool {
+        true
+    }
+
+    fn is_installed(self, package: &str) -> PackageInstalledMethod {
+        PackageInstalledMethod::Script(format!("zypper info {}", package))
+    }
+
+    fn known_flags_with_values(self) -> Vec<&'static str> {
+        vec!["-c", "--config", "-q", "--quiet"]
+    }
+
+    fn capture_flags(self) -> Vec<CaptureFlag> {
+        // TODO
+        vec![]
+    }
+
+    fn invalidating_flags(self) -> Vec<&'static str> {
+        vec![]
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Zypper;
+    use crate::{catch, package_manager::PackageManager};
+
+    #[test]
+    fn test_package_manager() {
+        let manager = PackageManager::from_line("zypper install test").unwrap();
+        assert_eq!(manager, PackageManager::from(Zypper));
+    }
+
+    #[test]
+    fn test_catch() {
+        // Regular invocation
+        catch!(PackageManager::from(Zypper), "sudo zypper install test" => "test");
+        catch!(PackageManager::from(Zypper), "zypper install test" => "test");
+        catch!(PackageManager::from(Zypper), "sudo zypper install libgfortran5-32bit" => "libgfortran5-32bit");
+        catch!(PackageManager::from(Zypper), "sudo zypper install perf" => "perf");
+
+        // Multiple
+        catch!(PackageManager::from(Zypper), "sudo zypper install test test2" => "test", "test2");
+
+        // Ignore
+        catch!(PackageManager::from(Zypper), "sudo zypper test test2" => ());
+        catch!(PackageManager::from(Zypper), "sudo zypper install -f" => ());
+    }
+}
